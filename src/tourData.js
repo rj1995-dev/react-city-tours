d38209f61df99d78d927385e0c42e228af10b74a
rj1995-dev <ram95.jana@gmail.com>
2019-08-12 03:22:37 +0530

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,50 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Bangalore",
+    img: "./Img/Bangalore.jpg",
+    name: "Bangalore bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 2,
+    city: "Delhi",
+    img: "./Img/Delhi.jpg",
+    name: "Delhi lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "Hydrabad",
+    img: "./Img/Hydrabad.jpg",
+    name: "Hydrabad royal palace tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "Kolkata",
+    img: "./Img/Kolkata.jpg",
+    name: "Kolkata bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 5,
+    city: "Pune",
+    img: "./Img/Pune.jpg",
+    name: "Pune temple tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 6,
+    city: "Mumbai",
+    img: "./Img/Mumbai.jpg",
+    name: "Mumbai bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
